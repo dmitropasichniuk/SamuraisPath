1d2cb020a20ae3ce4c0baeea14647f1f18821165
Dmytro <Dmytro>
2021-01-30 20:14:16 +0200

    first commit 13 lesson

---
+++ src/components/Profile.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Profile = () =>{
+ return (
+   <div className="content">
+     <div>
+       <img src="https://static3.depositphotos.com/1000454/256/i/600/depositphotos_2567474-stock-photo-wide-panorama-of-french-alps.jpg"></img>
+     </div>
+     <div>ava+description</div>
+     <div>
+       My posts
+       <div>New post</div>
+     </div>
+     <div>
+       <div>post 1</div>
+       <div>post 2</div>
+       <div>post 3</div>
+     </div>
+   </div>
+ );
+}
+
+export default Profile;
