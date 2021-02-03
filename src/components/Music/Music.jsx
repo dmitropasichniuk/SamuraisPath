ee78ea958f6de8b491a4cb88e9bd88de031baf8a
dmitropasichniuk <dimapasichniuk@gmail.com>
2021-02-03 18:51:00 +0200

    lesson 20 add music and settings module

---
+++ src/components/Music/Music.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "./Music.Module.css"
+
+const Music = () =>{
+    return(
+        <div>
+            Music
+        </div>
+    )
+}
+
+export default Music;
---
+++ src/components/News/News.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "./News.Module.css"
+
+const News = () =>{
+    return(
+        <div>
+            News
+        </div>
+    )
+}
+
+export default News;
