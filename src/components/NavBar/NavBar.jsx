007f3d377cdd832ea624577e888f12f45cc8713f
dmitropasichniuk <dimapasichniuk@gmail.com>
2021-02-04 00:13:28 +0200

    Add NavLincs fix

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,23 +1,24 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import styles from "./NavBar.Module.css"
 
 const NavBar = () =>{
  return (
    <nav className= {styles.nav}>
      <div className= {styles.item}>
-       <a href="/profile">Profile</a>
+       <NavLink to="/profile" activeClassName={styles.active}>Profile</NavLink>
      </div>
      <div className={styles.item}>  
-       <a href="/dialogs">Massages</a>
+       <NavLink to="/dialogs" activeClassName={styles.active}>Massages</NavLink>
      </div>
      <div className={styles.item}>
-       <a href="/news">News</a>
+       <NavLink to="/news" activeClassName={styles.active}>News</NavLink>
      </div>
      <div className={styles.item}>
-       <a href="/music">Music</a>
+       <NavLink to="/music" activeClassName={styles.active}>Music</NavLink>
      </div>
      <div className={styles.item}>
-       <a href="/settings">Settings</a>
+       <NavLink to="/settings" activeClassName={styles.active}>Settings</NavLink>
      </div>
    </nav>
  );
