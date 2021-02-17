7592f8013b559bb87377bd71887cafd0a223887a
dmitropasichniuk <dimapasichniuk@gmail.com>
2021-02-17 23:54:46 +0200

    add axios and first request to server

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,66 +1,66 @@
+import * as axios from "axios";
 import React from "react";
 import style from "./Users.Module.css"
+import userPhoto from "../../assets/images/usersIcon.jpg";
 
 const Users = (props) =>{
 
-  if(props.users.length === 0){
-    props.setUsers([
-      {
-        id: 1,
-        imgUrl: "https://i.pinimg.com/236x/74/05/5f/74055f83bfbdc20fdc1f9d1fc116fd26.jpg",
-        followed: true,
-        fullName: "Oleg",
-        status: "I am a boss",
-        location: { city: "Kiev", country: "Ukraine" },
-      },
-      {
-        id: 2,
-        imgUrl: "https://i.pinimg.com/236x/74/05/5f/74055f83bfbdc20fdc1f9d1fc116fd26.jpg",
-        followed: false,
-        fullName: "John",
-        status: "I am big boss",
-        location: { city: "Vinnitsia", country: "Ukraine" },
-      },
-      {
-        id: 3,
-        imgUrl: "https://i.pinimg.com/236x/74/05/5f/74055f83bfbdc20fdc1f9d1fc116fd26.jpg",
-        followed: true,
-        fullName: "Valentine",
-        status: "I am a little boss",
-        location: { city: "Arizona", country: "USA" },
-      },
-    ]);
-  }
-  
+  let showUsers = ()=> {
+    if (props.users.length === 0) {
+      axios
+        .get("https://social-network.samuraijs.com/api/1.0/users")
+        .then( response => {
+          props.setUsers(response.data.items);
+        });
+    }
+  };
 
-    return (
-      <div>
-        {props.users.map((u) => (
-          <div key={u.id}>
+  return (
+    <div className={style.items}>
+      <button onClick={showUsers}>Show users</button>
+      {props.users.map((u) => (
+        <div key={u.id}>
+          <span>
+            <div>
+              <img
+                src={u.photos.small != null ? u.photos.small : userPhoto}
+                className={style.userPhoto}
+              />
+            </div>
+            <div>
+              {u.followed ? (
+                <button
+                  onClick={() => {
+                    props.unfollow(u.id);
+                  }}
+                >
+                  Unfollow
+                </button>
+              ) : (
+                <button
+                  onClick={() => {
+                    props.follow(u.id);
+                  }}
+                >
+                  Follow
+                </button>
+              )}
+            </div>
+          </span>
+          <span>
             <span>
-              <div>
-                <img src={u.imgUrl} className={style.userPhoto}/>
-              </div>
-              <div>
-                {u.followed 
-                ? <button onClick={()=>{props.unfollow(u.id)}}>Unfollow</button> 
-                : <button onClick={()=>{props.follow(u.id)}}>Follow</button>}
-              </div>
+              <div>{u.name}</div>
+              <div>{u.status}</div>
             </span>
             <span>
-              <span>
-                <div>{u.fullName}</div>
-                <div>{u.status}</div>
-              </span>
-              <span>
-                <div>{u.location.city}</div>
-                <div>{u.location.country}</div>
-              </span>
+              {/* <div>{u.location.city}</div>
+                <div>{u.location.country}</div> */}
             </span>
-          </div>
-        ))}
-      </div>
-    );
+          </span>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Users;
